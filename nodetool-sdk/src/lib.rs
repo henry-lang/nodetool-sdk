9ebfe154be4b96d46007213d7227f732d0bc3888
Henry Langmack <henry.langmack@gmail.com>
2022-08-09 17:47:26 -0400

    cfg for reexport of nodes and macros in root crate

--- nodetool-sdk/src/lib.rs
+++ nodetool-sdk/src/lib.rs
@@ -1,2 +1,7 @@
-pub use nodetool_sdk_core as core;
+#[cfg(feature = "macros")]
 pub use nodetool_sdk_macros as macros;
+
+#[cfg(feature = "nodes")]
+pub use nodetool_sdk_nodes as nodes;
+
+pub use nodetool_sdk_core as core;
